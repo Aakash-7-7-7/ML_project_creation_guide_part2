619c93fabb3d9508c9621416c576e632bf64242e
Aakash-7-7-7 <niranjancharan75@gmail.com>
2025-11-16 12:35:49 +0530

    made data_transformation.py and added code in it then modified other files also

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from networksecurity.logging.logger import logging
 import sys
 
 from networksecurity.components.data_injestion import DataIngestion
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
-from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.logging.logger import logging
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig
 
+#from networksecurity.components.model_trainer import ModelTrainer
+#from networksecurity.entity.config_entity import ModelTrainerConfig
 
 
 if __name__=='__main__':
@@ -24,6 +28,12 @@ if __name__=='__main__':
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
        
         
 
